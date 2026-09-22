23c51b1929d212a8db1c8de86e91c0ef57bf7a8e
dezwashere <dezd8008@gmail.com>
2026-09-22 08:14:09 -0700

    add V1CE widget widgets/V1CEWidgetProps.ts

---
+++ widgets/V1CEWidgetProps.ts
@@ -0,0 +1,10 @@
+export type V1CEWidgetProps = {
+  days: number;
+  coinColor: string;
+  coinTextColor: string;
+  coinBorderColor: string;
+  coinShowBorder: boolean;
+  coinShape: string;
+  numberStyle: string;
+  motto: string;
+};
